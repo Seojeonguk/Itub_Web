c6e8d2ce65a63b925be2d588391e3ffb33ddc85f
Daein <dnfwlxo11@naver.com>
2020-05-04 02:03:22 +0900

    edit code

--- router/email/email.js
+++ router/email/email.js
@@ -5,7 +5,7 @@ var mysql = require('mysql')
 var path = require('path') // 상대경로 사용하는 모듈
 
 //mysql 접속 정보
-var connection = mysql.createConnection({
+var connection_infor = mysql.createConnection({
 	host: 'us-cdbr-iron-east-01.cleardb.net',
 	user: 'bf1138ba34c820',
 	password: '22ac05b88712768',
@@ -14,27 +14,29 @@ var connection = mysql.createConnection({
 });
 
 
-// db 접속 종료시 재연결
-function handleDisconnect(){
-	// 연결 되어있을시
-	connection.connect(function(err){
-		if (err){
-			console.log('db접속중', err)
-			// 서버 연결시간이 30초라 29초로 설정
-			setTimeout(handleDisconnect, 29000);
-		}
-	})
+// // db 접속 종료시 재연결
+// function handleDisconnect(){
+// 	// 연결 되어있을시
+// 	connection.connect(function(err){
+// 		if (err){
+// 			console.log('db접속중', err)
+// 			// 서버 연결시간이 30초라 29초로 설정
+// 			setTimeout(handleDisconnect, 29000);
+// 		}
+// 	})
 
-	// 연결 안되어있을시
-	connection.on('error', function(err){
-		console.log('db 에러', err);
-		if (err.code == 'PROTOCOL_CONNECTION_LOST'){
-			return handleDisconnect();
-		} else{
-			throw err;
-		}
-	})
-}
+// 	// 연결 안되어있을시
+// 	connection.on('error', function(err){
+// 		console.log('db 에러', err);
+// 		if (err.code == 'PROTOCOL_CONNECTION_LOST'){
+// 			return handleDisconnect();
+// 		} else{
+// 			throw err;
+// 		}
+// 	})
+// }
+
+//connection.connect(); // mysql 접속 명령어
 
 router.post('/form', function(req,res){
 	// getEx : req.param('email') 과 같이 사용
@@ -45,9 +47,8 @@ router.post('/form', function(req,res){
 });
 
 router.post('/ajax', function(req,res){
-	connection.connect(); // mysql 접속 명령어
-
-	console.log('잘되는중 ㅎㅎ')
+	var connection = connection_infor;
+	console.log('db 접속시도');
 	var email = req.body.email;
 	var responseDate = {};
 	var sql = 'SELECT u_name FROM u_id WHERE job=?';
