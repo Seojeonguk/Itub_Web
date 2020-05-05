fc1a737a326cd342a67f3c2eb78152a23f5eb997
Daein <dnfwlxo11@naver.com>
2020-05-05 20:39:03 +0900

    db func test

--- router/db/DBfunc.js
+++ router/db/DBfunc.js
@@ -19,7 +19,7 @@ router.post('/reference', function(req,res){
 	var connection = mysql.createConnection(dbconfig);
 
 	console.log('db 접속시도');
-	var data = req.body.nick_input;
+	var data = req.body.nick_name;
 	var responseDate = {};
 	var sql = 'SELECT u_name FROM u_id WHERE job=?';
 	console.log(data)
