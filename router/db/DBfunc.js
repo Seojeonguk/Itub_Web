887175c405bf28ebc5ce3d0500af911421746b66
Daein <dnfwlxo11@naver.com>
2020-05-05 21:15:18 +0900

    db func test

--- router/db/DBfunc.js
+++ router/db/DBfunc.js
@@ -74,7 +74,7 @@ router.post('/delete', function(req,res){
 		}
 
 		console.log(responseDate.result + ' ' + responseDate.u_name)
-		res.json({'지웠어':'지웠다니까'}); // 비동기이기 때문에 괄호안에 적어야함
+		res.json(responseDate); // 비동기이기 때문에 괄호안에 적어야함
 		connection.end();
 	})
 });
