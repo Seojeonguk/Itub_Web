1b551975c638628124536e6960ec5d6351b32d6a
Daein <dnfwlxo11@naver.com>
2020-05-07 18:47:18 +0900

    edit code

--- router/db/DBfunc.js
+++ router/db/DBfunc.js
@@ -7,15 +7,15 @@ var dbconfig = require('../../config.js')
 
 //connection.connect(); // mysql 접속 명령어
 
-router.post('/form', function(req,res){
+router.post('/form', function (req, res) {
 	// getEx : req.param('email') 과 같이 사용
 	console.log(req.body.data) // 서버에서 클라이언트로 데이터를 받음
 	res.send("<h1>Welcome!! " + req.body.email + "</h1>")
-	res.render('email.ejs', {'email': req.body.email}) 
+	res.render('email.ejs', { 'email': req.body.email })
 	// email.ejs에다가 다음과 같은 형태로 섞어 치환하여 클라이언트에 응답
 });
 
-router.post('/create', function(req,res){
+router.post('/create', function (req, res) {
 	var connection = mysql.createConnection(dbconfig);
 
 	console.log('db 접속시도');
@@ -24,10 +24,10 @@ router.post('/create', function(req,res){
 	var sql = 'INSERT INTO u_id(u_num, u_name, age, gender, job) VALUES (?, ?, ?, ?, ?)';
 	console.log(data)
 
-	var query = connection.query(sql, [100, data.u_name, parseInt(data.u_age), parseInt(data.u_gender), data.u_job], function(err,rows){
+	var query = connection.query(sql, [100, data.u_name, parseInt(data.u_age), parseInt(data.u_gender), data.u_job], function (err, rows) {
 
-		if(err) {
-			console.log("[mysql error]",err);
+		if (err) {
+			console.log("[mysql error]", err);
 			responseData.result = 'none'
 			responseData.type = 'create'
 			connection.end();
@@ -43,7 +43,7 @@ router.post('/create', function(req,res){
 });
 
 
-router.post('/reference', function(req,res){
+router.post('/reference', function (req, res) {
 	var connection = mysql.createConnection(dbconfig);
 
 	console.log('db 접속시도');
@@ -52,22 +52,24 @@ router.post('/reference', function(req,res){
 	var sql = 'SELECT * FROM u_id';
 	console.log(data)
 
-	var query = connection.query(sql, function(err,rows){
+	var query = connection.query(sql, function (err, rows) {
 
-		if(err) {
-			console.log("[mysql error]",err);
+		if (err) {
+			console.log("[mysql error]", err);
 			connection.end();
 		}
 
-		if(rows[0]){
+		if (rows[0]) {
 			responseData.type = 'reference';
 			responseData.result = 'ok';
-			responseData.u_num = rows.u_num;
-			responseData.age = rows.age;
-			responseData.gender = rows.gender;
-			responseData.job = rows.job;
-			console.log(rows.u_num + ' ' + rows.u_age + ' ' + rows.u_gender + ' ' + rows.u_job + ' ' );
-		} else{
+			for (var i = 0; i < rows.length; i++) {
+				responseData.u_num = rows[i].u_num;
+				responseData.age = rows[i].age;
+				responseData.gender = rows[i].gender;
+				responseData.job = rows[i].job;
+				console.log(rows[i].u_num + ' ' + rows[i].u_age + ' ' + rows[i].u_gender + ' ' + rows[i].u_job + ' ');
+			}
+		} else {
 			responseData.result = 'none';
 			responseData.type = 'reference';
 			console.log('none');
@@ -79,7 +81,7 @@ router.post('/reference', function(req,res){
 	})
 });
 
-router.post('/delete', function(req,res){
+router.post('/delete', function (req, res) {
 	var connection = mysql.createConnection(dbconfig);
 
 	console.log('db 접속시도');
@@ -88,11 +90,11 @@ router.post('/delete', function(req,res){
 	console.log(data)
 
 	var sql = 'SELECT * FROM u_id WHERE u_name=?';
-	var query = connection.query(sql, [data], function(err,rows){
-		if(rows[0]){
+	var query = connection.query(sql, [data], function (err, rows) {
+		if (rows[0]) {
 			responseData.result = 'ok';
 			responseData.type = 'delete'
-		} else{
+		} else {
 			responseData.result = 'none';
 			responseData.type = 'delete'
 			console.log('none : ' + data);
@@ -100,10 +102,10 @@ router.post('/delete', function(req,res){
 	});
 
 	sql = 'DELETE FROM u_id WHERE u_name=?';
-	var query = connection.query(sql, [data], function(err,rows){
+	var query = connection.query(sql, [data], function (err, rows) {
 
-		if(err) {
-			console.log("[mysql error]",err);
+		if (err) {
+			console.log("[mysql error]", err);
 			connection.end();
 		}
 
