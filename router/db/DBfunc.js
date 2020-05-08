db6e0797a6c8afcfbe4450f84cab774b14a23603
Daein <dnfwlxo11@naver.com>
2020-05-08 18:21:59 +0900

    edit code

--- router/db/DBfunc.js
+++ router/db/DBfunc.js
@@ -5,26 +5,16 @@ var mysql = require('mysql')
 var path = require('path') // 상대경로 사용하는 모듈
 var dbconfig = require('../../config.js')
 
-//connection.connect(); // mysql 접속 명령어
-
-router.post('/form', function (req, res) {
-	// getEx : req.param('email') 과 같이 사용
-	console.log(req.body.data) // 서버에서 클라이언트로 데이터를 받음
-	res.send("<h1>Welcome!! " + req.body.email + "</h1>")
-	res.render('email.ejs', { 'email': req.body.email })
-	// email.ejs에다가 다음과 같은 형태로 섞어 치환하여 클라이언트에 응답
-});
-
 router.post('/create', function (req, res) {
 	var connection = mysql.createConnection(dbconfig);
 
 	console.log('db 접속시도');
 	var data = req.body.data;
 	var responseData = {};
-	var sql = 'INSERT INTO u_id(u_num, u_name, age, gender, job) VALUES (?, ?, ?, ?, ?)';
+	var sql = 'INSERT INTO u_id(u_name, age, gender, job) VALUES (?, ?, ?, ?)';
 	console.log(data)
 
-	var query = connection.query(sql, [100, data.u_name, parseInt(data.u_age), parseInt(data.u_gender), data.u_job], function (err, rows) {
+	var query = connection.query(sql, [data.u_name, parseInt(data.u_age), parseInt(data.u_gender), data.u_job], function (err, rows) {
 
 		if (err) {
 			console.log("[mysql error]", err);
@@ -95,7 +85,7 @@ router.post('/delete', function (req, res) {
 		}
 	});
 
-	sql = 'DELETE FROM u_id WHERE u_name=?';
+	sql = 'DELETE FROM u_db WHERE u_num=?';
 	var query = connection.query(sql, [data], function (err, rows) {
 
 		if (err) {
