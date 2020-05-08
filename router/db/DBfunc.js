0051257bed587cd57a7ca9e7aa366853ea60dbfa
Daein <dnfwlxo11@naver.com>
2020-05-08 18:41:13 +0900

    edit code

--- router/db/DBfunc.js
+++ router/db/DBfunc.js
@@ -11,7 +11,7 @@ router.post('/create', function (req, res) {
 	console.log('db 접속시도');
 	var data = req.body.data;
 	var responseData = {};
-	var sql = 'INSERT INTO u_db(u_name, age, gender, job) VALUES (?, ?, ?, ?)';
+	var sql = 'INSERT INTO u_db(u_name, u_age, u_gender, u_job) VALUES (?, ?, ?, ?)';
 	console.log(data)
 
 	var query = connection.query(sql, [data.u_name, parseInt(data.u_age), parseInt(data.u_gender), data.u_job], function (err, rows) {
