e6cb578a46ef2ac597ae37eb29ca381db6624d58
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2020-06-23 19:58:25 +0900

    Update DBfunc.js

--- router/db/DBfunc.js
+++ router/db/DBfunc.js
@@ -7,7 +7,7 @@ var dbconfig = require('../../config.js')
 router.post('/create', function (req, res) {
     var connection = mysql.createConnection(dbconfig);
 
-    console.log('db 접속시도');
+    console.log('create db 접속시도');
     var data = req.body.data;
     var responseData = {};
     var sql = 'INSERT INTO u_db(u_name, u_age, u_gender, u_job) VALUES (?, ?, ?, ?)';
@@ -27,14 +27,27 @@ router.post('/create', function (req, res) {
 
         console.log(responseData)
         res.json(responseData); // 비동기이기 때문에 괄호안에 적어야함
+
+        sql = 'SELECT * FROM u_db';
+        connection.query(sql, function (err, rows, fields) {
+            if (err) {
+                console.log("[mysql error]", err);
+                connection.end();
+            } else {
+                for (var i = 0; i < rows.length; i++)
+                    console.log(rows[i].u_name + " : " + rows[i].u_num);
+            }
+        })
+
         connection.end();
     })
+
 });
 
 router.post('/reference', function (req, res) {
     var connection = mysql.createConnection(dbconfig);
 
-    console.log('db 접속시도');
+    console.log('reference db 접속시도');
     //var data = req.body.data;
     var responseData = {};
     var sql = 'SELECT * FROM u_db';
@@ -65,7 +78,7 @@ router.post('/reference', function (req, res) {
 router.post('/delete', function (req, res) {
     var connection = mysql.createConnection(dbconfig);
 
-    console.log('db 접속시도');
+    console.log('delete db 접속시도');
     var data = req.body.data;
     var responseData = {};
     console.log(data)
@@ -101,12 +114,12 @@ router.post('/delete', function (req, res) {
 router.post('/update', function (req, res) {
     var connection = mysql.createConnection(dbconfig);
 
-    console.log('db 접속시도');
-    var data = req.body.data;
+    console.log('update db 접속시도');
+    var data = req.cookies.u_num;
     var responseData = {};
     console.log(data)
 
-    var sql = 'SELECT * FROM u_db WHERE u_name=?';
+    var sql = 'SELECT * FROM u_db WHERE u_num=?';
     var query = connection.query(sql, [data], function (err, rows) {
         if (rows[0]) {
             responseData.result = 'ok';
@@ -120,7 +133,7 @@ router.post('/update', function (req, res) {
         }
     });
 
-    sql = 'UPDATE u_db set u_last = 1 where u_name=?';
+    sql = 'UPDATE u_db set u_last = 2 where u_num=?';
     var query = connection.query(sql, [data], function (err, rows) {
 
         if (err) {
@@ -138,6 +151,44 @@ router.post('/update', function (req, res) {
     })
 });
 
+router.post('/user_start', function (req, res) {
+
+    console.log('user_start db 접속시도');
+    var connection = mysql.createConnection(dbconfig);
+    var data = req.body;
+    var responseData = {};
+
+    var sql = 'SELECT * FROM u_db WHERE u_name=? AND u_age=? AND u_gender=? AND u_job=?';
+    console.log(data);
+    var query = connection.query(sql, [data.name, data.age, (data.gender == '남성' ? 0 : 1), data.job], function (err, rows) {
+        res.cookie('u_num', rows[0].u_num);
+        console.log(rows[0].u_num);
+        if (err) {
+            responseData.result = 'none';
+            responseData.type = 'user_start';
+            console.log("user_start error ", err);
+            connection.end();
+        }
+        var update = 'UPDATE u_db set u_last =1 where u_num=?'
+        var update_query = connection.query(update, [rows[0].u_num], function (err, rows) {
+            if (err) {
+                responseData.result = 'none';
+                responseData.type = 'user_start';
+                console.log("user_start error ", err);
+                connection.end();
+            }
+            responseData.result = 'ok';
+            responseData.type = 'user_start';
+            res.json(responseData);
+            connection.end();
+        })
+        responseData.result = 'ok';
+        responseData.type = 'user_start';
+        res.json(responseData);
+        connection.end();
+    })
+
+});
 
 
 module.exports = router;
